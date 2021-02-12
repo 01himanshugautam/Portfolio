c7adfbf4fec1c69b447070b51b4d5d1b6c922df6
Mahi <01himanshugautam@gmail.com>
2021-02-13 00:13:41 +0530

    Work In Progress

--- static/js/index.js
+++ static/js/index.js
@@ -1,27 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    window.addEventListener('scroll', myFunctionForSticky);
-    var navbar = document.getElementById("navbar");
-    var sticky = navbar.offsetTop;
-
-    function myFunctionForSticky() {
-        if (window.pageYOffset >= sticky) {
-            console.log("window.pageYOffset >= sticky");
-        } else {
-            console.log("Not window.pageYOffset >= sticky");
-        }
-        if (window.pageYOffset >= sticky) {
-            navbar.classList.add("sticky");
-        } else {
-            navbar.classList.remove("sticky");
-        }
-    }
-
-
-    function myFunctionForResponsive() {
-        navbar.classList.toggle('responsive');
-    }
-})
 var typed = new Typed("#animate", {
     strings: [
         "Web Developer",
